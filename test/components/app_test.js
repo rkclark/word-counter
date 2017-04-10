c2613ab86372b5a40b4cb41ff4ddecc16a48e29d
Rick Clark <rkingsleyclark@gmail.com>
2017-04-10 17:32:10 +0100

    Test that App renders Header and children

--- test/components/app_test.js
+++ test/components/app_test.js
@@ -1,16 +1,34 @@
 /* eslint-disable padded-blocks */
-
 import React from 'react';
 import { expect } from 'chai';
 import { shallow } from 'enzyme';
 import App from '../../src/app/components/App';
-
-const wrapper = shallow(<App />);
+import Header from '../../src/app/components/common/Header';
 
 describe('<App />', () => {
 
+  let child;
+  let props;
+  let wrapper;
+
+  beforeEach(() => {
+    child = <h1>Child</h1>;
+    props = {
+      children: child,
+    };
+    wrapper = shallow(<App {...props} />);
+  });
+
   it('renders successfully', () => {
     expect(wrapper).to.have.length(1);
+  });
+
+  it('renders a </Header />', () => {
+    expect(wrapper.contains(<Header />)).to.equal(true);
+  });
+
+  it('renders children passed as props', () => {
+    expect(wrapper.contains(child)).to.equal(true);
   });
 
 });
