bcbe3e3538a1bed69ab70caea42c5a605403cb6f
Rick Clark <rkingsleyclark@gmail.com>
2017-04-12 11:09:54 +0100

    Add initial isPrime function to PrimeStore

--- src/app/helpers/primeStore.js
+++ src/app/helpers/primeStore.js
@@ -1,7 +1,21 @@
 /* eslint-disable no-underscore-dangle, no-param-reassign */
 export default class PrimeStore {
   constructor() {
-    this._primes = {};
+    this._primes = [];
+  }
+
+  isPrime(int) {
+    for (let i = 2; i <= parseInt(Math.sqrt(int), 10); i++) {
+      if (int % i === 0) {
+        return false;
+      }
+    }
+    this._pushToPrimesArray(int);
+    return int > 1;
+  }
+
+  _pushToPrimesArray(int) {
+    this._primes.push(int);
   }
 
 }
--- test/helpers/primeStore_test.js
+++ test/helpers/primeStore_test.js
@@ -10,10 +10,36 @@ describe('PrimeStore', () => {
     primeStore = new PrimeStore();
   });
 
-  it('creates an empty _primes object on initialization', () => {
+  it('creates an empty _primes array on initialization', () => {
     const primes = primeStore._primes;
-    expect(primes).to.be.a('object');
-    expect(Object.keys(primes)).to.have.length(0);
+    expect(primes).to.be.a('array');
+    expect(primes).to.be.empty;
+  });
+
+  describe('#isPrime', () => {
+
+    it('returns true if number is prime', () => {
+      expect(primeStore.isPrime(37)).to.be.true;
+      expect(primeStore.isPrime(1021)).to.be.true;
+      expect(primeStore.isPrime(499)).to.be.true;
+    });
+
+    it('returns false if number is not prime', () => {
+      expect(primeStore.isPrime(36)).to.be.false;
+      expect(primeStore.isPrime(1022)).to.be.false;
+      expect(primeStore.isPrime(500)).to.be.false;
+    });
+
+    it('adds prime to primes array', () => {
+      primeStore.isPrime(37);
+      expect(primeStore._primes).to.contain(37);
+    });
+
+    it('does not add non-prime to primes array', () => {
+      primeStore.isPrime(36);
+      expect(primeStore._primes).not.to.contain(36);
+    });
+
   });
 
 });
